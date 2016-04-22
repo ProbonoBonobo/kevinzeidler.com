89fff3421aaaef21d5164b5ba3833df3429b5678
ProbonoBonobo <kzeidler@berkeley.edu>
2016-04-22 07:12:19 -0700

    mobile responsive design mostly done

--- vagrant/default/js/freelancer.js
+++ vagrant/default/js/freelancer.js
@@ -10,7 +10,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
 });
@@ -31,7 +31,7 @@ $('body').scrollspy({
     target: '.navbar-fixed-top',
     offset: window.innerHeight /2
 } );
-
+//
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
