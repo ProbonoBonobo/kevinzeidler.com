0b6dc9513933f8a2e9a446f3d470864837d4f355
ProbonoBonobo <kzeidler@berkeley.edu>
2016-04-22 09:35:41 -0700

    mobile scroll glitch patch attempt #1

--- vagrant/default/js/freelancer.js
+++ vagrant/default/js/freelancer.js
@@ -4,16 +4,18 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('body').on('click', '.page-scroll a', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1000, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+//$(function() {
+//    $('body').on('click', '.page-scroll a', function(event) {
+//        var $anchor = $(this);
+//        $('html, body').stop().animate({
+//            scrollTop: $($anchor.attr('href')).offset().top
+//        }, 1000, 'easeInOutExpo');
+//        event.preventDefault();
+//    });
+//});
 
 // Floating label headings for the contact form
 $(function() {
@@ -27,12 +29,12 @@ $(function() {
 });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top',
-    offset: window.innerHeight /2
-} );
+//$('body').scrollspy({
+//    target: '.navbar-fixed-top',
+//    offset: window.innerHeight /2
+//} );
 //
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+//// Closes the Responsive Menu on Menu Item Click
+//$('.navbar-collapse ul li a').click(function() {
+//    $('.navbar-toggle:visible').click();
+//});
