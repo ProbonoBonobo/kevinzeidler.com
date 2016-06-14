7a715925060a3d36de31d6b3ca236796eaf30691
Kevin's Server <kzeidler@gmail.com>
2016-06-13 23:18:05 -0400

    mhm

--- oops/index.php
+++ oops/index.php
@@ -17,9 +17,17 @@
     $site->setPage($page);
 
     $content = <<<EOT
-Greetings! My name's <img src="/img/php_bear.png">Glenby</a> and I'm a violent, PHP-smoking bear. I am a Lead Engineer at Yahoo! where I am responsible for the CSS backend, as well as for the spate of deadly bear attacks that have claimed the lives of at least twelve Yahoo! employees this year.
+Greetings! My name's <a href="/oops/img/phpbear.jpg">Glenby</a> and I'm a violent, PHP-smoking bear. I am a Lead Engineer at Yahoo! where I am responsible for the CSS backend, as well as for the spate of deadly bear attacks that have claimed the lives of at least twelve Yahoo! employees this year.
 
 <img src='img/php_bear.png'>
+<br><i><b>Summer 2001:</b> Freebasing rare earth metals with my friend Timothy Treadwell, star of Werner Herzog's uplifting comedy 'Grizzly Man' </i><br><br>
+
+<img src='img/phpbear.jpg'>
+<br><i><b>November 2014:</b> Quarterly shareholder meeting with Yahoo! CEO Marissa Meyer
+
+
+
+
 EOT;
     $page->setContent($content);
 
